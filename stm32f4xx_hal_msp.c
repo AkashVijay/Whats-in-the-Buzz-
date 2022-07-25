4c23a2529bd9abbb30ea5f735a22f7ab9bfc935c
AkashVijay <axvij1016@gmail.com>
2022-07-25 00:01:00 -0500

    Update stm32f4xx_hal_msp.c

--- stm32f4xx_hal_msp.c
+++ stm32f4xx_hal_msp.c
@@ -12,6 +12,7 @@
   *
   * COPYRIGHT(c) 2017 STMicroelectronics
   *
+  
   * Redistribution and use in source and binary forms, with or without modification,
   * are permitted provided that the following conditions are met:
   *   1. Redistributions of source code must retain the above copyright notice,
